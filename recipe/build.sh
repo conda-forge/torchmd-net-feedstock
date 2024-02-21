6323a973461d03ec12df099c5ae729d2fc2b89d4
RaulPPealez <raulppelaez@gmail.com>
2024-02-21 10:11:32 +0100

    add comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,7 @@ if [ ${cuda_compiler_version} == "None" ]; then
     export CPU_ONLY=1
 fi
 # Make the compilers look for headers in the conda include directory
+# I added this so that nvcc finds crypt.h, which is now installed separatedly from python
+# xref: https://github.com/conda-forge/linux-sysroot-feedstock/issues/52
 export CPATH=$PREFIX/include:$CPATH
 $PYTHON -m pip install . -vv
