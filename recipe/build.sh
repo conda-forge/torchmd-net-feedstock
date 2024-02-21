dcb5e6a594d2441cb70c977573e4299f6af2a2d8
RaulPPealez <raulppelaez@gmail.com>
2024-02-21 10:09:47 +0100

    Add PREFIX/include to CPATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 if [ ${cuda_compiler_version} == "None" ]; then
     export CPU_ONLY=1
 fi
-
+# Make the compilers look for headers in the conda include directory
+export CPATH=$PREFIX/include:$CPATH
 $PYTHON -m pip install . -vv
