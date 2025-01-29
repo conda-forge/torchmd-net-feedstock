63f3b6493b243618e29a09a57e1d7d40ca2653ca
Stefan Doerr <stefdoerr@gmail.com>
2025-01-29 13:53:32 +0200

    remove libtorch again, it was not the cause. try with different sed for OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,9 @@ fi
 
 # Fix duplicate include paths only for OSX ARM64
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "$target_platform" == "osx-arm64" ]]; then
-    # Remove duplicate torch include path from venv
-    export CFLAGS=$(echo $CFLAGS | sed 's|-I.*site-packages/torch/include||g')
-    export CXXFLAGS=$(echo $CXXFLAGS | sed 's|-I.*site-packages/torch/include||g')
+    # Remove duplicate torch include paths from venv
+    export CFLAGS=$(echo $CFLAGS | sed 's|-I.*site-packages/torch/include\(/.*\)*||g')
+    export CXXFLAGS=$(echo $CXXFLAGS | sed 's|-I.*site-packages/torch/include\(/.*\)*||g')
 fi
 
 $PYTHON -m pip install . -vv
