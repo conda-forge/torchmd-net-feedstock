69152e6ef86d15a4d082a82dce6a55601a6c4d89
Stefan Doerr <stefdoerr@gmail.com>
2025-01-29 11:53:02 +0200

    try removing one of the two include paths in OSX Arm64 to fix the issue with the duplicate definition of OrderedDict

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,11 @@ if [ ${cuda_compiler_version} == "None" ]; then
     export CPU_ONLY=1
 fi
 
+# Fix duplicate include paths only for OSX ARM64
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "$target_platform" == "osx-arm64" ]]; then
+    # Remove duplicate torch include path from venv
+    export CFLAGS=$(echo $CFLAGS | sed 's|-I.*site-packages/torch/include||g')
+    export CXXFLAGS=$(echo $CXXFLAGS | sed 's|-I.*site-packages/torch/include||g')
+fi
+
 $PYTHON -m pip install . -vv
