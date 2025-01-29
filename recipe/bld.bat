b62b639e3d5f3768989f7088e0544441df38cc52
Stefan Doerr <stefdoerr@gmail.com>
2025-01-29 11:30:07 +0200

    for some reason libtorch dep solves space issues???

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,11 +4,6 @@ if "%cuda_compiler_version%"=="None" (
     set CPU_ONLY=1
 )
 
-:: Make the compilers look for headers in the conda include directory
-:: I added this so that nvcc finds crypt.h, which is now installed separatedly from python
-:: xref: https://github.com/conda-forge/linux-sysroot-feedstock/issues/52
-set CPATH=%PREFIX%\include;%CPATH%
-
 %PYTHON% -m pip install . -vv
 
 goto :EOF
