65eb771c995f061c088e58e98f325299d34a2ef7
Stefan Doerr <stefdoerr@gmail.com>
2025-02-27 15:52:59 +0200

    change variable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,12 @@
 @echo on
 
 IF "%cuda_compiler_version%"=="None" (
-    SET WITH_CUDA="0"
+    SET WITH_CUDA=0
 ) ELSE (
-    SET WITH_CUDA="1"
+    SET WITH_CUDA=1
 )
 
-echo "WITH_CUDA: %WITH_CUDA%"
+echo WITH_CUDA: %WITH_CUDA%
 
 %PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 
