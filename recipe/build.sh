4efc01505c027b21d68da421bc2f829a1d78cc9f
Stefan Doerr <stefdoerr@gmail.com>
2025-04-30 14:19:17 +0300

    define acceleratorl

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 
 export WITH_CUDA=$([ "${cuda_compiler_version}" == "None" ] && echo "0" || echo "1")
+export ACCELERATOR=$([ "${cuda_compiler_version}" == "None" ] && echo "cpu" || echo "cu${cuda_compiler_version}")
 
-echo "WITH_CUDA: $WITH_CUDA"
+echo "ACCELERATOR: $ACCELERATOR"
 ${PYTHON} -m pip install . -vv --no-build-isolation --no-deps
