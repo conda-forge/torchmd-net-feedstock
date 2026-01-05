154774fbaea9ca5101f3d887604d54e074d59900
Stefan Doerr <stefdoerr@gmail.com>
2026-01-05 10:56:56 +0200

    clean grayskull version

--- recipe/build.sh
+++
@@ -1,6 +0,0 @@
-
-export WITH_CUDA=$([ "${cuda_compiler_version}" == "None" ] && echo "0" || echo "1")
-export ACCELERATOR=$([ "${cuda_compiler_version}" == "None" ] && echo "cpu" || echo "cu${cuda_compiler_version}")
-
-echo "ACCELERATOR: $ACCELERATOR"
-${PYTHON} -m pip install . -vv --no-build-isolation --no-deps
